c5e1da6d8e0eb906b4a51b63707778e6cd540ab7
Sophie <szaniszlozs7@gmail.com>
2020-04-28 13:48:13 +0200

    add fields and methods to PassengerService

--- src/com/train/PassengerService.java
+++ src/com/train/PassengerService.java
@@ -1,4 +1,22 @@
 package com.train;
 
 public class PassengerService extends Train {
+
+    protected int minimumPassengersNumber;
+    protected int maximumPassengersNumber;
+    protected double ticketPrice;
+    protected double onTrainTicketSurcharge;
+    protected int chancePunishTravelers;
+
+    protected double punishTravelers() {
+        return 0.0;
+    }
+
+    protected double buyTicketsOnTrain() {
+        return 0.0;
+    }
+
+    protected int simulateDailyPassengerNumber(Month) {
+        return 0;
+    }
 }
